ea59e4efd2d5b75f2ba469eee0416549b446382e
wickidcow <jaywago@gmail.com>
2026-09-04 21:57:37 -0400

    compat: use Legacy SlimefunItemStack directly for mutant drops

--- src/main/java/io/github/addoncommunity/galactifun/base/aliens/MutantCreeper.java
+++ src/main/java/io/github/addoncommunity/galactifun/base/aliens/MutantCreeper.java
@@ -36,7 +36,7 @@ public final class MutantCreeper extends Alien<Creeper> {
     @Override
     public void onDeath(@Nonnull EntityDeathEvent e) {
         e.getDrops().clear();
-        e.getDrops().add(new SlimefunItemStack(BaseMats.MUNPOWDER, ThreadLocalRandom.current().nextInt(2)).item());
+        e.getDrops().add(new SlimefunItemStack(BaseMats.MUNPOWDER, ThreadLocalRandom.current().nextInt(2)));
     }
 
     @Override
@@ -59,5 +59,4 @@ public final class MutantCreeper extends Alien<Creeper> {
             }
         }
     }
-
 }
